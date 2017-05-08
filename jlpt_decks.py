e490cd6cf2540e20ca721dca22f55fc446a1779b
Jithesh E J <mail@jithesh.net>
2017-05-08 13:05:29 +0530

    Add task to clean up the tweets
    Add ProcessTweets task to clean up the tweets
    and create a line delimited JSON with
    the question, options and the answer key.

--- jlpt_decks.py
+++ jlpt_decks.py
@@ -1,3 +1,4 @@
+import re
 import json
 import datetime
 import luigi
@@ -45,6 +46,81 @@ class GetTweets(luigi.Task):
                         ensure_ascii=False
                     )
                 )
+                outfile.write('\n')
+
+
+class ProcessTweets(luigi.Task):
+    """
+    Generates a local file to be written to a database for persistent
+    storage after cleaning up the tweets
+    """
+    task_namespace = 'twitter'
+    date = luigi.DateParameter(default=datetime.date.today())
+
+    def requires(self):
+        return GetTweets()
+
+    def output(self):
+        return luigi.LocalTarget(
+            path='/tmp/_processed_tweets-%s.ldj' % self.date
+        )
+
+    @staticmethod
+    def _tidy(question):
+        item = dict()
+        pattern = re.compile(
+            pattern=r'([a-zA-Z])\s?\.?\s?(.*?)(?=[a-zA-Z]|$)',
+            flags=re.IGNORECASE
+        )
+        ques = question.strip()
+        # Check if options are available for the question
+        index = ques.rfind('A')
+        if index == -1:
+            stem = re.sub(pattern=r'\s+', repl=' ', string=ques.strip())
+            item.update({'stem': stem})
+            return item
+        # Clean up options as key: value pairs
+        stem = re.sub(pattern=r'\s+', repl=' ', string=ques[:index].strip())
+        item.update({'stem': stem})
+        options = ques[index:].strip()
+        options = dict(pattern.findall(string=options))
+        item.update(
+            {'options': {k.strip(): v.strip() for k, v in options.items()}}
+        )
+        return item
+
+    def run(self):
+        # An item bank to function as a repository of test items
+        item_bank = list()
+        # `characters` and regex `pattern` to check
+        # the presence of in a tweet
+        characters = '◇◆'
+        pattern = re.compile(r'([^◇◆]+)', flags=re.IGNORECASE)
+
+        with self.input().open('r') as fobj:
+            for t in fobj.readlines():
+                tweet = json.loads(s=t)
+                text = tweet.get('text')
+                if text and any(char in text for char in characters):
+                    question, answer = pattern.findall(string=text)
+                    key = answer.strip()
+                    # Clean up `question`
+                    item = self._tidy(question=question)
+                    options = item.get('options')
+                    if options and options.get(key):
+                        item.update(
+                            {'key': '{key_index}. {key}'
+                                .format(key_index=key, key=options.get(key))}
+                        )
+                    else:
+                        item.update({'key': key})
+                    item.update(tweet)
+                    item_bank.append(item)
+
+        # Write `item_bank` to line delimited JSON
+        with self.output().open('w') as outfile:
+            for item in item_bank:
+                outfile.write(json.dumps(item, ensure_ascii=False))
                 outfile.write('\n')
 
 
